e49a14128a283359b480532b0f5ae7e2a5c6bfb1
ALUNO <ALUNO@DESKTOP-EL61SJM>
2023-01-31 19:37:35 -0300

    modificado branch hotfix

--- GCMSPraticaGit/src/HelloWord.java
+++ GCMSPraticaGit/src/HelloWord.java
@@ -5,5 +5,8 @@ public class HelloWord {
 		 System.out.println("Hello World");
 		 System.out.println("Olá mundo");
 		 System.out.println("Modificações");
+		 System.out.println("Pratica 4, parte 1 - passo 2");
+		 System.out.println("branch - hotfix");
+		 
 	 }
 }
